9310917907d990a529b785c656caa95bdd17f8e3
Ayyappa Swamy <panthadiayyappa@gmail.com>
2019-01-27 14:53:59 +0530

    Tested every method(API urls)  with sample word

--- dict.js
+++ dict.js
@@ -10,28 +10,67 @@ var headers = {
 };
 
 
-//Method to get word definition
-function getDefinition(word){
+//Method to get word definitions
+function getDefinitions(word){
     var url = 'https://api.wordnik.com/v4/word.json/'+word+'/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=c23b746d074135dc9500c0a61300a3cb7647e53ec2b9b658e';
-    return definitions;
+
+    requestApi(url, function(error, json_data){
+        if(!error){
+            console.log(json_data);
+        }
+        else{
+            console.log("There is something problem with wordnik API. Try again sometime.\n");
+        }
+    });
+    //return definitions;
 }
 
 //Method to get word synonyms
 function getSynonyms(word){
-i   var url = 'https://api.wordnik.com/v4/word.json/'+word+'/relatedWords?useCanonical=false&relationshipTypes=synonym&limitPerRelationshipType=10&api_key=c23b746d074135dc9500c0a61300a3cb7647e53ec2b9b658e';
-    return synonyms;
+   var url = 'https://api.wordnik.com/v4/word.json/'+word+'/relatedWords?useCanonical=false&relationshipTypes=synonym&limitPerRelationshipType=10&api_key=c23b746d074135dc9500c0a61300a3cb7647e53ec2b9b658e';
+    
+    requestApi(url, function(error, json_data){
+        if(!error){
+            console.log(json_data);
+        }
+        else{
+            console.log("There is something problem with wordnik API. Try again sometime.\n");
+        }
+    });
+
+    //return synonyms;
 }
 
 //Method to get word antonyms
 function getAntonyms(word){
     var url = 'https://api.wordnik.com/v4/word.json/'+word+'/relatedWords?useCanonical=false&relationshipTypes=antonym&limitPerRelationshipType=10&api_key=c23b746d074135dc9500c0a61300a3cb7647e53ec2b9b658e';
-    return antonyms;
+
+    requestApi(url, function(error, json_data){
+        if(!error){
+            console.log(json_data);
+        }
+        else{
+            console.log("There is something problem with wordnik API. Try again sometime.\n");
+        }
+    });
+
+    //return antonyms;
 }
 
 //Method to get word examples
 function getExamples(word){
     var url = 'https://api.wordnik.com/v4/word.json/'+word+'/examples?includeDuplicates=false&useCanonical=false&limit=5&api_key=c23b746d074135dc9500c0a61300a3cb7647e53ec2b9b658e';
-    return examples;
+
+    requestApi(url, function(error, json_data){
+        if(!error){
+            console.log(json_data);
+        }
+        else{
+            console.log("There is something problem with wordnik API. Try again sometime.\n");
+        }
+    });
+
+    //return examples;
 }
 
 //Method to get full information about word
@@ -45,13 +84,33 @@ function getFullDict(word){
 //Method to get word of the day
 function getWOD(){
     var url = 'https://api.wordnik.com/v4/words.json/wordOfTheDay?api_key=c23b746d074135dc9500c0a61300a3cb7647e53ec2b9b658e';
-    return word;
+
+    requestApi(url, function(error, json_data){
+        if(!error){
+            console.log(json_data);
+        }
+        else{
+            console.log("There is something problem with wordnik API. Try again sometime.\n");
+        }
+    });
+
+    //return word;
 }
 
 //Method to get random word
 function getRandomWord(){
     var url = 'https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=c23b746d074135dc9500c0a61300a3cb7647e53ec2b9b658e';
-    return word;
+
+    requestApi(url, function(error, json_data){
+        if(!error){
+            console.log(json_data);
+        }
+        else{
+            console.log("There is something problem with wordnik API. Try again sometime.\n");
+        }
+    });
+
+    //return word;
 }
 
 //Method to play word game
@@ -61,23 +120,31 @@ function play(){
 }
 
 //Method to call api
-function requestApi(url, callback){
-    request(url, headers, function callback(error, response, body) {
+function requestApi(url, cb){
+    request(url, function callback(error, response, body) {
+        //console.log(response);
         if (!error && response.statusCode == 200) {
-            #console.log(response.status);
             var data = JSON.parse(body);
-            callback(data);
+            //console.log(data);
+            cb(null, data);
         }
         else{
-            console.log("Something is wrong!!!");
-            console.log(response.status);
-            console.log(error);
-            callback();
+            console.log("\n\nSomething is wrong!!!");
+            //console.log(response.status);
+            cb(error, null);
         }
-    }
+    });
 }
 
 
 
 var args = process.argv.slice(2);
 
+word = 'freedom';
+
+//getDefinitions(word);
+//getSynonyms(word);
+//getAntonyms(word);
+//getExamples(word);
+//getWOD();
+getRandomWord();
